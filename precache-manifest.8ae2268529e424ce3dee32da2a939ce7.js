47610a7ed777683c68231be8996a0fd9523be0bd
AnsurM <ansurmehdi@gmail.com>
2020-01-05 21:23:49 +0500

    Updates

---
+++ precache-manifest.8ae2268529e424ce3dee32da2a939ce7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c17743ba81bba804ed757c580129b86a",
+    "url": "/ReactHooksTodoApp/index.html"
+  },
+  {
+    "revision": "57cd67ca9b47e16482a4",
+    "url": "/ReactHooksTodoApp/static/css/main.7c0cb491.chunk.css"
+  },
+  {
+    "revision": "cc0ce3721ca4fc51f00c",
+    "url": "/ReactHooksTodoApp/static/js/2.7376034b.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/ReactHooksTodoApp/static/js/2.7376034b.chunk.js.LICENSE"
+  },
+  {
+    "revision": "57cd67ca9b47e16482a4",
+    "url": "/ReactHooksTodoApp/static/js/main.b87082cb.chunk.js"
+  },
+  {
+    "revision": "93eeebbd1271299c743e",
+    "url": "/ReactHooksTodoApp/static/js/runtime-main.d919a286.js"
+  }
+]);
